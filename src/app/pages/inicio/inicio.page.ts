bdc6198c733db8750f9da368481dab4e5a03b54c
wiwis <djerizooriginal@gmail.com>
2022-09-14 13:28:12 -0500

    Practica 4

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Componente{
+  icon:string;
+  name:string;
+  redirectTo:string;
+  }
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
 
+export class InicioPage implements OnInit {
+  componentes: Componente[]=[
+    {
+    icon:'american-football-outline',
+    name:'action sheet',
+    redirectTo:'/action-sheet'
+    },
+    {
+    icon:'logo-apple-appstore',
+    name:'alert',
+    redirectTo:'/alert'
+    }
+    ];
   constructor() { }
 
   ngOnInit() {
