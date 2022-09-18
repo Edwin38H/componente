35dd17c69ff0f465b06470e42c1b8c18b0fbd71a
wiwis <djerizooriginal@gmail.com>
2022-09-17 21:32:06 -0500

    Practica 5

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ActionSheetController} from '@ionic/angular';
 @Component({
   selector: 'app-action-sheet',
   templateUrl: './action-sheet.page.html',
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private actionSheetCtrl:ActionSheetController) { }
+  onClick(){
+    this.presentActionSheet();
+    }
   ngOnInit() {
   }
 
-}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+    header: 'Album',
+    cssClass: 'my-custom-class',
+    buttons: [{
+    text: 'Delete',
+    role: 'destructive',
+    icon: 'trash-outline',
+    cssClass:'rojo',
+    handler: () => {
+    console.log('Delete clicked');
+    }
+  }, {
+    text: 'Share',
+    icon: 'share-outline',
+    handler: () => {
+    console.log('Share clicked');
+    }
+    }, {
+    text: 'Play (open modal)',
+    icon: 'caret-forward-circle',
+    handler: () => {
+    console.log('Play clicked');
+    }
+    }, {
+    text: 'Favorite',
+    icon: 'heart-outline',
+    handler: () => {
+    console.log('Favorite clicked');
+    }
+  }, {
+    text: 'Cancel',
+    icon: 'close-outline',
+    role: 'cancel',
+    handler: () => {
+    console.log('Cancel clicked');
+    }
+    }]
+    });
+    await actionSheet.present();
+    }
+    }
+  
+
