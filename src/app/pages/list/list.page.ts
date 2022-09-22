b785f47865d084140ff322a7d3f77f3a587a2773
wiwis <djerizooriginal@gmail.com>
2022-09-22 10:42:06 -0500

    practica 12

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataService} from '../../services/data.service';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
@@ -8,6 +10,7 @@ import { DataService} from '../../services/data.service';
 })
 export class ListPage implements OnInit {
   usuarios:Observable<any>;
+  @ViewChild(IonList)ionList:IonList;
   constructor(private dataService:DataService) { }
   
 
@@ -15,5 +18,17 @@ export class ListPage implements OnInit {
     //this.dataService.getUsuarios().subscribe(console.log);
 this.usuarios=this.dataService.getUsuarios();
   }
+  favorite(user:any){
+    console.log('favorite',user);
+    this.ionList.closeSlidingItems();
+    }
+    share(user:any){
+    console.log('share',user);
+    this.ionList.closeSlidingItems();
+    }
+    delete(user:any){
+    console.log('delete',user.name);
+    this.ionList.closeSlidingItems();
+    }
 
 }
