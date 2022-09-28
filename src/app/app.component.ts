cade72fd20b82aa76bc4e0e65356d7c1db66909e
wiwis <djerizooriginal@gmail.com>
2022-09-28 09:39:27 -0500

    Practica 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { Componente } from './interfaces/interfaces';
+import { Observable } from 'rxjs';
+import { DataService } from './services/data.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+  componentes: Observable<Componente[]>;
+  constructor(
+    private platform: Platform,
+    private dataService: DataService
+  ) {
+    this.initializeApp();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.componentes = this.dataService.getMenuOpts();
+    });
+  }
 }
