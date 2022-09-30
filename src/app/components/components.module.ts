9f3fe67fda0a5233c40c0c0ed025f4fc41429f25
wiwis <djerizooriginal@gmail.com>
2022-09-30 10:31:14 -0500

    Practica 20

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
-
-
+import { PopoverInfoComponent } from './popover-info/popover-info.component';
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    PopoverInfoComponent
+  ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    PopoverInfoComponent
   ],
   imports: [
     CommonModule,
